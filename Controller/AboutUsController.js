6f1f95319c1beca647c4afcfccf37b350b06419b
Guilherme Menezes <78366324+Neoxepa@users.noreply.github.com>
2024-11-24 22:15:23 -0300

    Adição das redes, necessario tornar clicável

--- Controller/AboutUsController.js
+++ Controller/AboutUsController.js
@@ -2,14 +2,14 @@ const mapauser = new Map();
 
 const user1 = new Map();  
 user1.set("nome", "Guilherme Menezes");
-user1.set("github", "https://github.com/Neoxepa");
-user1.set("linkedin", "?");
+user1.set("github", "Neoxepa");
+user1.set("linkedin", "https://www.linkedin.com/in/guilherme-menezes-4558a3234/");
 user1.set("curso", "Engenharia da computação");
 
 const user2 = new Map();
 user2.set("nome", "Cauã Verissimo");
-user2.set("github", "?");
-user2.set("linkedin", "?");
+user2.set("github", "Kc1fer");
+user2.set("linkedin", "https://www.linkedin.com/in/cauã-verissimo-359248323/");
 user2.set("curso", "Engenharia da computação");
 
 const user3 = new Map();
@@ -20,20 +20,20 @@ user3.set("curso", "Ciência da computação");
 
 const user4 = new Map();
 user4.set("nome", "Kaique Lemos");
-user4.set("github", "?");
-user4.set("linkedin", "?");
+user4.set("github", "Kaique-Lemos");
+user4.set("linkedin", "https://www.linkedin.com/in/kaique-lemos-21b83031b");
 user4.set("curso", "Ciência da computação");
 
 const user5 = new Map();
 user5.set("nome", "Matheus Hustenir");
-user5.set("github", "?");
-user5.set("linkedin", "?");
+user5.set("github", "MHustenir");
+user5.set("linkedin", "https://www.linkedin.com/in/matheus-hustenir-286925326/");
 user5.set("curso", "Ciência da computação");
 
 const user6 = new Map();
 user6.set("nome", "Bruno Nogueira");
-user6.set("github", "?");
-user6.set("linkedin", "?");
+user6.set("github", "DEGUSTEBR");
+user6.set("linkedin", "https://www.linkedin.com/in/bruno-nogueira-da-rocha-1ab05033a");
 user6.set("curso", "Ciência da computação");
 
 mapauser.set("menezes", user1);
